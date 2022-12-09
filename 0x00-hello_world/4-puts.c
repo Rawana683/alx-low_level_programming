e81f643cbbd970a22c81ffd9d2e8ecbf22f495a9
Rawan Sayed <rawansayed81@gmail.com>
2022-12-08 18:46:29 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/*
+/**
 *main - Entry point
 *Description: 'this program is about printing string using puts function'
-*Return: Always 0
+*Return: Always 0 (Success)
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
