5d6489227ece0f10697805016b18321acaddd4cd
Rawan Sayed <rawansayed81@gmail.com>
2022-12-10 11:44:04 -0800

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
 *main - Entry point
 *Description: ' this program is to print a quote'
-* Return : Always (1) (Success)
+*Return : Always 1 (Success)
 */
 int main(void)
 {
 fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-61, 1, stdout);
+59, 1, stdout);
 return (1);
 }
