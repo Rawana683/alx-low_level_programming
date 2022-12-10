e47c8e0da09877ed3a7ae6d23e72cb16dfd42775
Rawan Sayed <rawansayed81@gmail.com>
2022-12-09 16:23:34 -0800

    alphabet

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,9 +8,9 @@ int main(void)
 
 {
 char alpha;
-for ( alpha = 'a' ; alpha <= 'z' ; alpha++ )
+for (alpha = 'a' ; alpha <= 'z' ; alpha++)
 {
-if( alpha != 'q' || alpha != 'e')
+if (alpha != 'q' && alpha != 'e')
 { putchar(alpha); }
 }
 putchar('\n');
