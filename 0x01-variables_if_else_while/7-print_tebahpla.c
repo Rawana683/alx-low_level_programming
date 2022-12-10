c2b1e6c08ca7b448d08d1951f6a254f3c6a536c5
Rawan Sayed <rawansayed81@gmail.com>
2022-12-09 17:28:49 -0800

    alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description; 'this program is to print alphabet'
+*Return: Always (0) (Success)
+*/
+int main(void)
+{
+char c;
+for (c = 'z' ; c >= 'a' ; c++)
+{ putchar(c); }
+putchar('\n');
+return (0);
+}
