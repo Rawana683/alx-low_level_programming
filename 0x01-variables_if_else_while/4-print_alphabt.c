52d495eeec238f1db8db105e67b800f86b92032d
Rawan Sayed <rawansayed81@gmail.com>
2022-12-09 16:03:59 -0800

    alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*Description: ‘this program is to print alphabet ‘
+* Return: Always 0 (Success)
+*/
+int main(void)
+
+{
+char alpha;
+do {putchar(alpha); }
+while (alpha != 'q' || alpha != 'e')
+putchar('\n');
+return (0);
+}
