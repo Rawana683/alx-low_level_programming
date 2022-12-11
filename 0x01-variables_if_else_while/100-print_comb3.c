74e47da56144db58b642386b8ea1092a71bba17d
Rawan Sayed <rawansayed81@gmail.com>
2022-12-11 06:09:07 -0800

    numbers combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Description: 'this program is to print compined numbers'
+*Return : Always (0) (Success)
+*/
+int main(void)
+{
+	int n, i;
+	for (n = 48, i = 58 ; n < 58, i > 48 ; n++, i--)
+{
+	putchar(ni);
+	putchar(in);
+if (n != 57 || i != 57)
+{
+	purchar(',');
+	putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
+
