3f07dbd27881fcce4b9f22e5314a5e659e59160b
Rawan Sayed <rawansayed81@gmail.com>
2022-12-11 09:23:50 -0800

    numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'printing all compinations'
+ * Return : Always(0) (Success)
+*/
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 48 ; a < 58 ; a++)
+	{
+		for (b = 49 ; b < 58 ; b++)
+		{
+			for (c = 50 ; c < 58 ; c++)
+			{
+				if (b == c)
+				{
+					continue;
+				}
+				if (a == b)
+				{
+					continue;
+				}
+				putchar(a);
+				putchar(b);
+				putchar(c);
+				if (a == 55 && b == 56 && c == 57)
+				{
+					break;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
