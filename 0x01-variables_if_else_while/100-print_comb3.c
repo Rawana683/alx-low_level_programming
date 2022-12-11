a9e1091451c29b9d6c99ef4eeb5c5d2bfc30cd8f
Rawan Sayed <rawansayed81@gmail.com>
2022-12-11 06:59:13 -0800

    num

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int n[10];
 	int i[10];
-	for (n[0] = 48, i[0] = 57 ; n < 58 && i > 48 ; n++, i--)
+	for (n[0] = 48, i[0] = 57 ; n[10] < 58 && i[10] > 48 ; n++, i--)
 {
 	putchar(n[], i[]);
 	putchar(i[], n[]);
