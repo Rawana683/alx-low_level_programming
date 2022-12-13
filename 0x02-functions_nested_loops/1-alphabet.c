ab193a59a0e771037e11570eb354501862f4acc2
Rawan Sayed <rawansayed81@gmail.com>
2022-12-13 06:59:53 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,11 @@
 #include "main.h"
 /**
- * print_alphabet - Entry point
+ * main - Entry point
  * Description 'this program is to print alphabet'
  * Return: Always 0 (Success)
 */
-int print_alphabet(void)
+int main(void)
 {
-	int n;
-
-	for (n = 48 ; n < 58 ; n++)
-	{
-		_putchar(n);
-	}
-	_putchar('\n');
+	print_alphabet();
 	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,12 @@
 int _putchar(char);
 void print();
 void print_alphabet(void);
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 48 ; n < 58 ; n ++)
+		_putchar(n);
+	_putchar('\n');
+	return (0);
+}
