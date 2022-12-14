256610988868116be20a4d2015f788216d50ef06
Rawan Sayed <rawansayed81@gmail.com>
2022-12-14 13:43:28 -0800

    positive or negative

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,15 +11,18 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
-		printf("-1\n");
+		_putchar('-');
+		return (-1);
 	}
 	else if (n == 0)
 	{
-		printf("00\n");
+		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		printf("+1\n");
+		_putchar('+');
+		return (1);
 	}
-	return (0);
+	_putchar('\n');
 }
