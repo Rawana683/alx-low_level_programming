517914ae3abbc80ec8d8b52d9a7a670ece523a6c
Rawan Sayed <rawansayed81@gmail.com>
2022-12-14 14:14:48 -0800

    last digit ptonter

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_last_digit - Entry point
+ * @n : the number to check
+ * Description: ' function to print last digit'
+ * Return: last digit of the function
+ */
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10
+	return (l);
+}
