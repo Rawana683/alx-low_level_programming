57f5b5ba043f6268807548fda29c6977349af8e3
Rawan Sayed <rawansayed81@gmail.com>
2022-12-14 12:55:06 -0800

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,8 +3,9 @@
 #include <ctype.h>
 /**
  * _islower - Entry point
+ * @c : the number to be checked
  * Description : 'functon to recognize lowercase letters'
- * Return : 0. (Success)
+ * Return: int of 1 if lower . 0 otherwise. (Success)
  */
 int _islower(int c)
 {
