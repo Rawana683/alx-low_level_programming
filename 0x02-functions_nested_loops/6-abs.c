b8ff260bc92971672f5dcd9efb2b6b0dae4a556d
Rawan Sayed <rawansayed81@gmail.com>
2022-12-14 13:52:42 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _abs - Entry point
+ * @c : the number to check
+ * Description: 'function to print the absolute value'
+ * Return: the absolute value of int. (Success)
+ */
+int _abs(int c)
+{
+	abs(c);
+	return (c);
+}
