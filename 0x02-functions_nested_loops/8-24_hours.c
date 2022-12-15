83deff67b5d22866b075ee7809db5877ec742d01
Rawan Sayed <rawansayed81@gmail.com>
2022-12-15 06:33:16 -0800

    minutes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * jack_bauer -success Entry point
+ * Description9 : 'printing the minutes '
+ * Return: 0. (Success)
+ */
+void jack_bauer(void)
+{
+	int h;
+	int m;
+
+	for ( h = 0 ; h < 24 ; h++)
+	{
+		for (m = 0 ; m <= 59 ; m++)
+		{
+			printf("0%i:0%i", h, m);
+			_putchar('\n');
+		}
+	}
+	return (0);
+}
