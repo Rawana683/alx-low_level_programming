d4101de4d798e090c09a614317c7f1d16561a169
Rawan Sayed <rawansayed81@gmail.com>
2022-12-15 07:56:31 -0800

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - Entry point
+ *@a : first intger
+ *@b : second intger
+ * Description: 'adding two intgers'
+ * Return: the result of addition
+ */
+int add(int a, int b)
+{
+	int add;
+	
+	add = a + b
+	return (add);
+}
