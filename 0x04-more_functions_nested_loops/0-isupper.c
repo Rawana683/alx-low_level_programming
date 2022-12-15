c7bc2e440df72d7c58f542f1d3ad74e2204bf7eb
Rawan Sayed <rawansayed81@gmail.com>
2022-12-15 11:57:01 -0800

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "maiin.h"
+#include <stdio.h>
+
+/**
+ * _isupper - Entry point
+ *@c : number to check
+ *Description : 'function to chech upper case'
+ *Return: 1 if it is upper case. other wise 0. (Success)
+ */
+int _isupper(int c)
+{
+	if (isupper(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
