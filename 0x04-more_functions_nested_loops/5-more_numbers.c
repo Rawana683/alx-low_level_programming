6a343dada6c1e5ba50ad89dcb35aa60e9636acd2
Rawan Sayed <rawansayed81@gmail.com>
2022-12-15 15:56:21 -0800

    n

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,24 +8,20 @@
  */
 void more_numbers(void)
 {
-	int n;
-	int c;
+	int n, i;
 
-	for (n = 0; n < 10 ; n++)
+	for (n = 0; n <= 10; n++)
 	{
-		for (c = 0 ; c < 15 ; c++)
-		{
-			if  (c < 10)
-			{
-			_putchar(c);
-			}
-			else if (c >= 10)
-			{
-				_putchar('1');
-				_putchar(c % 10 + '0');
-			}
-		}
-		printf("\n");
+	for (i = 0; i <= 14; i++)
+	{
+	if (i >= 10)
+	{
+	_putchar(i / 10 + '0');
 	}
-
+	else
+	{
+	_putchar(i % 10 + '0');
+	}
+	}
+	_putchar('\n');
 }
