16d192b69841c85098da0687e232abc9827fcf15
Rawan Sayed <rawansayed81@gmail.com>
2022-12-15 08:09:30 -0800

    l

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_to_98 - entry point
+ *@n : number entered by user
+ * Description: 'printing natural numbers'
+ * Return: 0.
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+	while (n < 98)
+	{
+		printf("%d", n);
+		_putchar(',');
+		_putchar(' ');
+		n++
+	}
+	}
+	else
+	{
+		while (n > 98)
+		{
+			printf("%d", n);
+			_putchar(',');
+			_putchar(' ');
+			n--
+		}
+	}
+
