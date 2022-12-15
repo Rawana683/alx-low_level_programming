9e49ea67954230affc20133124836a764d6ee13f
Rawan Sayed <rawansayed81@gmail.com>
2022-12-15 12:36:31 -0800

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ *mul - entry point
+ *@a : number to multiply
+ *@b : number to multiply
+ *Description: 'multiply numbers'
+ * Return: the result (Success)
+ */
+int mul(int a, int b)
+{
+	m = a * b
+		return (m);
+}
