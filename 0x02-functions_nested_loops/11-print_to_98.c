98c643243604e791ee9fdb52e6fbad5126ed4055
Rawan Sayed <rawansayed81@gmail.com>
2022-12-15 08:40:57 -0800

    l

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,21 +11,18 @@ void print_to_98(int n)
 	if (n < 98)
 	{
 	while (n <= 98)
-	{
-		printf(" %i,", n);
-		putchar(',');
-		putchar(' ');
+	{	printf("%d, ", n);
 		n++;
 	}
+	printf("%d\n", n);
 	}
 	else
 	{
 		while (n > 98)
 		{
-			putchar(n + '0');
-			putchar(',');
-			putchar(' ');
+			printf("%d, ", n);
 			n--;
 		}
+		printf("%d\n", n);
 	}
 }
