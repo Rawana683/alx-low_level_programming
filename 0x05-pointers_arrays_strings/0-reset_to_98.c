01f6a19dc36cdaefc31309455e9d3104f22f4226
Rawan Sayed <rawansayed81@gmail.com>
2022-12-20 09:21:25 -0800

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *reset_to_98 - entry point
+ * @*n : the pointer
+ * Descrtiption: 'using pointer'
+ * Return : 0. (Success)
+ */
+void reset_to_98(int *n)
+{
+	int m;
+
+	*n = &m;
+	*n = 98;
+}
