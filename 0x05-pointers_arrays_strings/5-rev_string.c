f56112f084aadee422b029ed8c4b1a0517fc5e67
Rawan Sayed <rawansayed81@gmail.com>
2022-12-20 13:30:14 -0800

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * rev_string - entry point
+ * @s : char to use
+ * Description: ' reverse string'
+ * Return: 0. (Success)
+ */
+void rev_string(char *s)
+{
+	int len;
+	int o;
+	int i;
+
+	len = 0;
+	o = 0;
+
+	for (i = 0; s[i] != '\0' ; i++)
+	{
+		len = i;
+	}
+	for (; len >= o; len--)
+	{
+		_putchar(s[len]);
+	}
+}
