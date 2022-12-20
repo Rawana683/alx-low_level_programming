327dcce868b8ae663052db2a1bd6b41af6533664
Rawan Sayed <rawansayed81@gmail.com>
2022-12-20 12:16:05 -0800

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - entry point
+ * @s : char to use
+ * Description: 'print a string in reverse'
+ * Return: 0. (Success)
+ */
+void print_rev(char *s)
+{
+	char a[15];
+	int i;
+
+	for (i = 15 ; i >= 0; i--)
+	{
+		puts(a[i]);
+	}
+}
