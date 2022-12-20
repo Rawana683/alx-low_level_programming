3c6776bee959394c543f1b007940ee0299e5da90
Rawan Sayed <rawansayed81@gmail.com>
2022-12-20 14:04:12 -0800

    rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 /**
  * rev_string - entry point
  * @s : char to use
@@ -9,26 +10,13 @@
 void rev_string(char *s)
 {
 	int len;
-	int o;
 	int i;
 
-	len = 0;
-	o = 0;
-
-	for (i = 0; s[i] != '\0' ; i++)
-	{
-		len = i;
-	}
-	if (len == 1)
-	{
-		_putchar(' ');
-	}
-	for (; len >= o; len--)
+	len = strlen(s) + 1;
+	i = 0;
+	while (len > i)
 	{
 		_putchar(s[len]);
-		if (len == i)
-		{
-			break;
-		}
+		i++;
 	}
 }
