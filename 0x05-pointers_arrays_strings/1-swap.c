cedf39037418bbd6289c9d17e38094656f9e952a
Rawan Sayed <rawansayed81@gmail.com>
2022-12-20 09:41:49 -0800

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *swap_int - entry point
+ * @a : number 1
+ * @b : number 2
+ * Description : ' swaping numbers '
+ * Return: 0. (Success)
+ */
+void swap_int(int *a, int *b)
+{
+	int one;
+	int two;
+	int *first;
+	int *second;
+
+	first = &two;
+	second = &one;
+	*a = *first;
+	*b = *second;
+}
