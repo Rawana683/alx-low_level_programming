f06b6a6637b9945f50d77ddb32291ee2923fba6b
Rawan Sayed <rawansayed81@gmail.com>
2022-12-20 14:52:04 -0800

    l

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,11 +11,15 @@ void puts2(char *str)
 {
 	int i;
 
-	i = 0;
-	_putchar(str[i]);
 	for (i = 0; str[i] != '\0' ; i++)
 	{
-		i = i + 2;
-		_putchar(str[i]);
+		if ((i % 2) == 0)
+		{
+			_putchar(str[i]);
+		}
+		else
+		{
+			continue;
+		}
 	}
 }
