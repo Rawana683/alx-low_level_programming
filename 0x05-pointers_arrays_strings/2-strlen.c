861a7b0871e397733e29b560faf4d262b995f610
Rawan Sayed <rawansayed81@gmail.com>
2022-12-20 11:52:25 -0800

    lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - entry point
+ * @s : the char to use0
+ * Description: 'print the lenght of string'
+ * Return: the lenght (success)
+ */
+int _strlen(char *s)
+{
+	int l;
+
+	l = strlen(s);
+	return (l);
+}
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @i : the char to use
+ * Description : 'putchar'
+ * Return: 0. (Success)
+ */
+int _putchar(char i)
+{
+	putchar(i);
+	return(i);
+}
