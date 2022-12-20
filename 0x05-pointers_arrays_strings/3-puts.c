afe502f6065f7e54101967a18e50fdeabf944860
Rawan Sayed <rawansayed81@gmail.com>
2022-12-20 11:59:46 -0800

    string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - entry point
+ * @str : the char to use
+ * description : 'printing a string'
+ * Return: 0. (Success)
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
