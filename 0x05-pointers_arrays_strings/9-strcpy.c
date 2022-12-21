8de13eef85d2edbcfd3dc70719eb93bb376c4aa6
Rawan Sayed <rawansayed81@gmail.com>
2022-12-20 16:07:16 -0800

    l

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcpy - entry point
+ * @dest : char to use
+ * @src :  char to use
+ * Desription: 'pointer to pointer
+ * Return: the value of dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+}
