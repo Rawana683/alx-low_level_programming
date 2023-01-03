dfecfa272d3790d74d486c4ef2dbf25c35b748e8
Rawan Sayed <rawansayed81@gmail.com>
2023-01-02 18:13:11 -0800

    d

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_diagsums - entry point
+ *@a : number to use
+ *@size : number to use
+ * Description : " diagonal sum in matrix"
+ *Return: 0.
+ */
+void print_diagsums(int *a, int size)
+{
+	
