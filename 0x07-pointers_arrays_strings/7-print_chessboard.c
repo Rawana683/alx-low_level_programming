11e61402ec20ce0aa88b726f585c3a9257830082
Rawan Sayed <rawansayed81@gmail.com>
2023-01-02 18:06:47 -0800

    chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_chessboard - entry point
+ * @a : arrray of chars
+ * Description : "print chessboard"
+ * Return: 0.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, n;
+
+	for (i = 0 ; i < 8 ; i++)
+	{
+		for (n = 0 ; n < 8 ; n++)
+		{
+			_putchar(a[i][n]);
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * _putchar - entry point
+ * @i : char to use
+ * Description "putchar used to print"
+ * Return: 0.
+ */
+int _putchar(char i)
+{
+	putchar(i);
+	return(i);
+}
