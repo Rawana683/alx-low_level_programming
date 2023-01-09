8ae5d8bfba56f761229389e45d1905416c49c0f8
Rawan Sayed <rawansayed81@gmail.com>
2023-01-09 05:54:55 -0800

    ststic

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,55 +1,40 @@
 #include "main.h"
 
-
-
 /**
- *
- *  * _atoi - convert a string to an integer
- *
- *   * @s: pointer to a string
- *
- *    * Return: integer
- *
- *     */
-
-
+ * _atoi - convert a string to an integer
+ * @s: pointer to a string
+ *Description : "convert string to int"
+ * Return: integer
+ */
 
 int _atoi(char *s)
-
 {
-
 	int i = 0, n = 0, sign = 1;
 
 	while ((s[i] < '0' || s[i] > '9') && s[i] != '\0')
-
 	{
-
 		if (s[i] == '-')
-
 		{
-
 			sign *= -1;
-
 		}
 
 		i++;
-
 	}
 
 	while ((s[i] >= '0' && s[i] <= '9') && s[i] != '\0')
 
 	{
 		if (n >= 0)
-	{
-		n = n * 10 - (s[i] - '0');
-		i++;
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
+		else
+		{
+			n = n * 10 - (s[i] - '0');
+			i++;
+		}
 	}
-	else
-	{
-		n = n * 10 - (s[i] - '0');
-		i++;
-	}
-}
-sign *= -1;
-return (n *sign);
+	sign *= -1;
+	return (n * sign);
 }
