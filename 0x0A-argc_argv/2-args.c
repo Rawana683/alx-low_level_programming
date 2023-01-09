a197cc8cc48eff49e2ea5665f841616c5ed1de44
Rawan Sayed <rawansayed81@gmail.com>
2023-01-09 14:11:49 -0800

    arg

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc : number to use
+ * @argv : array of arg
+ * Description : " print all arguments"
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc ; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
