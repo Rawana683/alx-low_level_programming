879cfb1185337823479e8dc2e638cc1298387db7
Rawan Sayed <rawansayed81@gmail.com>
2023-01-09 14:33:04 -0800

    a

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,14 +9,14 @@
  */
 int main(int argc, char *argv[])
 {
-	if (argc >= 0)
-	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
-		return (0);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error");
 		return (1);
 	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	return (0);
 }
