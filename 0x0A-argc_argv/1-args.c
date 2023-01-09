fabc8b6c2a63706356ce9fadf644afcfee129740
Rawan Sayed <rawansayed81@gmail.com>
2023-01-09 14:01:49 -0800

    argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc : number of arg
+ * @argv : argument vector
+ * Description : "print number of arguments"
+ * Return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
