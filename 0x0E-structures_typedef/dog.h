d86b150f8f9db9db7a3560d4caac19e1f77595ca
Rawan Sayed <rawansayed81@gmail.com>
2023-01-16 14:08:01 -0800

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef RO
+#define RO
+/**
+ * struct dog - entry point
+ * @name : char of name
+ * @age : int of ge
+ * @owner : char of name
+ * Description : " struct dog to say name age and owner"
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
