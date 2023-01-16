55ffab13d503565b139ca9ac938c6d1a3e94a261
Rawan Sayed <rawansayed81@gmail.com>
2023-01-16 08:04:46 -0800

    macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ * Description : "print the name of the file"
+ * Return: 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+}
