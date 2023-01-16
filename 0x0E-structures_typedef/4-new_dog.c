af0a523d0bdffba77040d2e9b62389a82de97f3b
Rawan Sayed <rawansayed81@gmail.com>
2023-01-16 15:00:25 -0800

    free

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * new_dog - entry point
+ * @name : char to use
+ * @age : int to use
+ * @owner: char to use
+ * Description : "new dog created"
+ * Return: new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
