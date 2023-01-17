bd4abb59a798573e9da1e3290439e2ee95271105
Rawan Sayed <rawansayed81@gmail.com>
2023-01-17 06:24:30 -0800

    l

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,7 +12,41 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *r_dog;
-	dog_t *k_dog;
+	int i, n, o;
 
-	memcpy(r_dog, k_dog,sizeof(dog_t));
+	r_dog = malloc(sizeof(*r_dog));
+	if (r_dog = NULL || name = NULL || owner = NULL)
+	{
+		return (NULL);
+		free(r_dog);
+	}
+	for (n = 0; name[n]; n++)
+	{
+		continue;
+	}
+	for (o = 0; owner[o]; o++)
+	{
+		continue;
+	}
+	r_dog->name = malloc(n + 1);
+	r_dog->owner = malloc(o + 1);
+	if (!(r_dog->name) || !(r_dog->owner))
+	{
+		free(r_dog->name);
+		free(r_dog->owner);
+			free(r_dog);
+		return (NULL);
+	}
+	for (i = 0; i < n; i++)
+	{
+		r_dog->name[i] = name[i];
+	}
+	r_dog->name[i] = '\0';
+	r_dog->age = age;
+	for (i = 0; i < o; i++)
+	{
+		r_dog->owner[i] = owner[i];
+	}
+	r_dog->owner[i] = '\0';
+	return (r_dog);
 }
