f8897c5c304d24d2b8f32040449b8170e0558e46
Rawan Sayed <rawansayed81@gmail.com>
2023-01-19 13:53:59 -0800

    f

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,6 +23,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			break;
 		}
+		if ( s == NULL)
+		{
+			printf("(nil)");
+		}
 		printf("%c ", *separator);
 	}
 	printf("\n");
