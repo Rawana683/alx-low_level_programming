b9c09bdead269b87e703e51cb54e9be30ec354e3
Rawan Sayed <rawansayed81@gmail.com>
2023-01-19 14:41:57 -0800

    l

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,18 @@
+#ifndef RO
+#define RO
+#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+typedef struct prints
+{
+		char *notation;
+		void (*print_dType)(va_list argptr);
+} prints_t;
+
+void print_char(va_list argptr);
+void print_int(va_list argptr);
+void print_float(va_list argptr);
+void print_string(va_list argptr);
+#endif 
