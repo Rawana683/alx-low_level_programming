84371d1fec1da7c6c71ea8c3004377eeeac535ff
Rawan Sayed <rawansayed81@gmail.com>
2023-01-19 08:54:23 -0800

    f

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,3 +1,5 @@
+#ifndef RO
+#define RO
 #include <stdio.h>
 /**
 * struct op - Struct op
@@ -15,3 +17,4 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+#endif
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,26 +9,26 @@
  * description : "simple calculator"
  * Return: result
  */
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 	int a, b, result;
-	
+
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit (98);
+		exit(98);
 	}
-	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*' 
+	if (*argv[2] != '+' && *argv[2] != '-' && *argv[2] != '*'
 			&& *argv[2] != '/' && *argv[2] != '%')
 	{
 		printf("Error\n");
-		exit (99);
+		exit(99);
 	}
-	if ((*argv[2] = '/' && atoi(argv[3]) == 0)
-			|| (*argv[2] = '%' && atoi(argv[3]) == 0))
+	if (((*argv[2] = '/') && (atoi(argv[3]) == 0))
+			|| ((*argv[2] = '%') && (atoi(argv[3]) == 0)))
 	{
 		printf("Error\n");
-		exit (100);
+		exit(100);
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
