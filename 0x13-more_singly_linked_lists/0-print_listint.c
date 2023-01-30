371a4807edb5959c56c3617c8ed848aec34f7660
Rawan Sayed <rawansayed81@gmail.com>
2023-01-30 06:38:55 -0800

    lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,13 +1,15 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ * print_listint - print elements
+ * @h : our struct
+ * description : "print elements and return count"
+ * Return: no of elements
  */
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
+
 	while (h)
 	{
 		if (h != NULL)
