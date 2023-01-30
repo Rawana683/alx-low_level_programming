6a3f728b199a87f96a26d2941cba0ec07b9da440
Rawan Sayed <rawansayed81@gmail.com>
2023-01-30 06:57:50 -0800

    linked lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *
+ *
+ *
+ *
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
