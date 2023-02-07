78f917baca986001d522699c63aa00daac33374d
Rawan Sayed <rawansayed81@gmail.com>
2023-02-07 06:10:21 -0800

    fio

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * read_textfile - read text file
+ * @filename: file name
+ * @letters: size of letters
+ * description : "read and print file and return letters"
+ * Return: no of letters or 0 on fail
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	o = open(filename, O_RDONLY);
+	r = read(o, buf, letters);
+	w = (STDOUT_FILENO, buf, r);
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	close(o);
+	return (w);
+}
